f46609c2c4efec1f28b9743ded9195b4851582ce
Your Name <you@example.com>
2024-11-14 14:44:13 +0700

    update data

--- public/assets/js/reponsive.js
+++ public/assets/js/reponsive.js
@@ -1,9 +1,16 @@
 // Lấy phần tử menu-button và canvas-menu
-const menuButton = document.querySelector('.menu-button');
-const canvasMenu = document.querySelector('.canvas-menu');
+const menuButton = document.querySelector('#menu-btn');
+const menuButtonx = document.querySelector('#button-x')
+const canvasMenu = document.querySelector('.nav-mobile');
 
 // Bắt sự kiện khi nhấn vào menu-button
 menuButton.addEventListener('click', () => {
     // Thêm lớp active để hiện canvas menu
     canvasMenu.classList.toggle('active');
+    canvasMenu.classList.toggle('start');
+});
+
+menuButtonx.addEventListener('click',()=>{
+    canvasMenu.classList.remove('active')
+    canvasMenu.classList.remove('start')
 });
